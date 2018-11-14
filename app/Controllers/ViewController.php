dbc5cb48c28e0c004d54c592665f6228ba5df671
Alina Salinas Mendoza <alinasalmen995@gmail.com>
2018-11-14 12:06:25 -0600

    Agregando los controladores del Autoload, el controlador para las vistas y el de los usuarios

---
+++ app/Controllers/ViewController.php
@@ -0,0 +1,13 @@
+<?php
+
+class ViewController{
+    private static $view_path = './App/Views/';
+
+    public function loadView( $view ){
+            require_once(self::$view_path .'Head.php');
+            require_once(self::$view_path . 'Header.php');
+            require_once(self::$view_path . $view . '.php');
+            require_once(self::$view_path . 'Footer.php');
+    }
+
+}
