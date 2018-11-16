27b3b3c9a9b0eac5b84b9cb2e5e9c17bbcac32cc
AlinaSM <alinasalmen995@gmail.com>
2018-11-16 14:15:18 -0600

    Terminando la vista del login

--- app/Views/login.php
+++ app/Views/login.php
@@ -1,10 +1,24 @@
 <div class="container-fluid">
-    <div class="row">
-        <h1>Inicio de Sesion</h1>
+    <h6 class="text-center">Inicio de Sesion</h6>
+
+    <div class="row  justify-content-center align-items-center">
+        <form action="" method="POST">
+           <div class="form-group">
+                <label for="">Nombre de Usuario</label>
+                <input type="text" class="form-control form-control-sm" name="txtNombre">
+           </div>
+           <div class="form-group">
+                <label for="">Contraseña</label>
+                <input type="password" class="form-control form-control-sm" name="txtPassword">
+           </div>
+            <div class="row justify-content-center align-items-center">
+                <input type="submit" value="Entrar" class="btn btn-primary btnEntrar">
+            </div>
+        </form>
     </div>
-    <div class="row">
-        <div class="col-4"></div>
-        <div class="col-4"></div>
-        <div class="col-4"></div>
+    
+    <div class="row justify-content-center align-items-center float-right">
+        <input type="button" id="btnEntrarInvitado" value="Ingresar como invitado" class="btn btn-primary btnEntrar btnInv">
     </div>
+    
 </div>
