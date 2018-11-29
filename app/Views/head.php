1fcad36ca80303b8e123e21c76d9eb7a6e60e45b
AlinaSM <alinasalmen995@gmail.com>
2018-11-28 22:53:39 -0600

    Integrando la parte de diana, el buscador

--- app/Views/head.php
+++ app/Views/head.php
@@ -6,8 +6,10 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     
     <link rel='stylesheet' href='../css/bootstrap.min.css'>
+    <link rel="stylesheet" href="../css/presenta.css">
     <link rel='stylesheet' href='../css/styles.css'>
     <link rel="stylesheet" type="text/css" href="../css/main.css">
+    
 
     <title>Biblioteca Virtual</title>
 </head>
